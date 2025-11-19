85bc45988121d59755c088acb2a986269f7001a4
bhaskar-experion <bhaskar.nandagopal@experionglobal.com>
2025-11-19 10:14:58 +0530

    restricted egress of vpc endpoint to vpc cidr

--- modules/vpc/endpoints.tf
+++ modules/vpc/endpoints.tf
@@ -102,7 +102,7 @@ resource "aws_security_group" "vpc_endpoint_sg" {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    cidr_blocks     = [var.network_cidr]
     description     = "Allow all outbound traffic"
   }
 
